60ca9c8486d9646d2353672c72332a573b274c88
Kenny Diaz <kend77@gmail.com>
2018-07-15 21:16:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
       return `you have ${item.itemName} at $${item.itemPrice}`
     }
   })
-  return cart.length ? `In your cart, ${items.join(' ,')}.` : 'Your shopping cart is empty.'
+  return cart.length ? `In your cart, ${items.join(', ')}.` : 'Your shopping cart is empty.'
 }
 
 function total() {
