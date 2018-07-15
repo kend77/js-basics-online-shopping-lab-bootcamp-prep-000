809efbb0448b74af5ff684d5cad49f92e9de8ead
Kenny Diaz <kend77@gmail.com>
2018-07-15 21:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,20 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  cart.push({ itemName: item, price: Math.floor(Math.random() * 100)})
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  const items = cart.map((item, idx) => {
+    if (idx === (cart.length - 1)) {
+      return `and ${item.itemName} at $${item.price}.`
+    } else {
+      return `you have ${item.itemName} at $${item.price}`
+    }
+  })
+  return cart.length ? `In your cart you have, ${items.join(',')}` : 'Your shopping cart is empty.'
 }
 
 function total() {
