2e8980dd8a1edfe6502c8b8f6de4628354fa2a17
Kenny Diaz <kend77@gmail.com>
2018-07-15 21:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ function viewCart() {
   // write your code here
   const items = cart.map((item, idx) => {
     if (cart.length > 1 && idx === (cart.length - 1)) {
-      return `and ${item.itemName} at $${item.itemPrice}.`
+      return `and ${item.itemName} at $${item.itemPrice}`
     } else {
       return `you have ${item.itemName} at $${item.itemPrice}`
     }
   })
-  return cart.length ? `In your cart, ${items.join(',')}` : 'Your shopping cart is empty.'
+  return cart.length ? `In your cart, ${items.join(',')}.` : 'Your shopping cart is empty.'
 }
 
 function total() {
